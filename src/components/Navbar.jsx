2339850219916ab484e081aa57d9f29ab60241ee
Nakul <nakulbhat034@gmail.com>
2023-11-12 21:35:22 +0530

    Partially finished work on Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../../public/assets/airbnblogo.png";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <img src={logo} alt="AirBnb Logo" className="w-10" />
+      text
+    </nav>
+  );
+};
+
+export default Navbar;
