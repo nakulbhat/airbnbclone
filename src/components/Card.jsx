1d552d106f066a2366146a6e8b23687ac86e088d
Nakul <nakulbhat034@gmail.com>
2023-11-15 17:01:04 +0530

    Fixed images issue

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 import img from "../assets/image.png";
 import staricon from "../assets/staricon.png";
-const Card = ({title, image, country, price, reviewnumber}) => {
-  let img = './assets/' + image;
+const Card = ({ title, image, country, price, reviewnumber }) => {
+
   console.log(img);
   return (
     <div className="m-10 mt-0 w-[263px]">
-      <img src={img} className="w-[176px]"/>
+      <img src={`/src/assets/${image}`} className="w-[176px]" />
+
       <div className="flex justify-start align-middle text-xs ml-1 py-1">
         <img src={staricon} className="h-[14px] " />
         <p className="px-1">
-          5.0 <span className="text-gray-400">({reviewnumber}) &#x2022; {country} </span>
+          5.0{" "}
+          <span className="text-gray-400">
+            ({reviewnumber}) &#x2022; {country}{" "}
+          </span>
         </p>
       </div>
-      <p className="mx-2 text-xs font-light ">
-        {title}
-      </p>
+      <p className="mx-2 text-xs font-light ">{title}</p>
       <p className=" mx-2 font-light  text-[12px]">
         <div className="font-bold inline-block">From ${price}</div> / person
       </p>
